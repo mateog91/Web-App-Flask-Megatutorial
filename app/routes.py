59c613464087d1c818c6e4df5ec69b3340c03077
Mateo <mateog91@gmail.com>
2022-05-18 13:19:17 -0500

    ADD: mock post as dictionary structure, add the login endpoint for get and post methods.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -14,9 +15,23 @@ def index():
         {
             'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'
-        }
+        },
+        {
+            'author': {'username': 'Manuel'},
+            'body': 'Hey bro! I live in Berlin now!!!!'
+        },
+        {
+            'author': {'username': 'Mateo'},
+            'body': 'I\'m going to live to Toronto!!'
+        },
     ]
-
-
-
     return render_template('index.html', title='Casa', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+        form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
